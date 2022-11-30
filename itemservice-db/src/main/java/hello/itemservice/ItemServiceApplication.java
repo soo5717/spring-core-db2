9735bd2e2ecd41798a7d28c22cc6bf9b2463e924
Suyeon <koo5717@naver.com>
2022-11-30 19:50:24 +0900

    데이터 접근 기술 - 테스트 (#8)
    * :wrench: 테스트 데이터소스 설정
    
    * :white_check_mark: 데이터 롤백
    
    * :white_check_mark: @Transactional
    
    * :hammer: schema.sql 생성
    
    * :recycle: 임베디드 모드 DB
    
    * :wrench: 테스트 데이터소스 설정 제거
    
    * :recycle: 스프링 임베디드 모드

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,12 +2,14 @@ package hello.itemservice;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
+@Slf4j
 //@Import(MemoryConfig.class)
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
